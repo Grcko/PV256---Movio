d56608330ba9df0b30601b0689e24a40ea29ccaa
Oliver Pentek <oliver.pentek@gmail.com>
2015-10-05 09:33:05 +0200

    Static class for constants  and API key

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco396100/Constants.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv256.movio.uco396100;
+
+/**
+ * Created by oliver on 5.10.2015.
+ */
+public final class Constants {
+    public static final String API_KEY = "8b69cec294461a500ad80660782c8b91";
+
+    private Constants() {
+    }
+}
